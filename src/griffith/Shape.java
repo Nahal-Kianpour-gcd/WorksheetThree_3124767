00638b3aefb4f500ec10b0d21858f77bede78dd5
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-03-14 17:09:24 +0000

    Added abstract Shape class with area and perimeter methods

--- src/griffith/Shape.java
+++ src/griffith/Shape.java
@@ -1,5 +1,31 @@
 package griffith;
 
 public abstract class Shape {
+	private String name;
 
+	// Constructor
+	public Shape(String name) {
+		this.name = name;
+	}
+
+	// Getter for name
+	public String getName() {
+		return name;
+	}
+
+	// Setter for name
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// Abstract methods
+	public abstract double area();
+
+	public abstract double perimeter();
+
+	// toString method
+	@Override
+	public String toString() {
+		return "Shape: " + name;
+	}
 }
